6002b15ff355e4554a287cf03fef1d85dc714711
Dmytro <subaru9.dev@proton.me>
2025-04-10 13:09:51 +0300

    feat(rate_limit): atomic counter check and increment

--- apps/lol_api/lib/lol_api/rate_limiter/redis_command.ex
+++ apps/lol_api/lib/lol_api/rate_limiter/redis_command.ex
@@ -3,6 +3,10 @@ defmodule LolApi.RateLimiter.RedisCommand do
   Domain-aware redis commands
   """
 
+  @type keys :: list(String.t())
+  @type ttls :: list(non_neg_integer())
+  @type limit_check_result :: {:allowed} | {:throttled, non_neg_integer()}
+
   @doc """
   Atomic operation with Lua script needed for rate limiter.
   """
@@ -25,5 +29,58 @@ defmodule LolApi.RateLimiter.RedisCommand do
       # will be ARGV[1] in Lua script
       to_string(ttl)
     ]
+  end
+
+  def check_keys_existance(keys) when is_list(keys) do
+    [
+      "EXISTS" | keys
+    ]
+  end
+
+  @doc """
+  Atomically checks rate limit counters against their limits, and updates them if allowed.
+
+  ## Arguments
+
+  - `counter_keys`: Redis keys tracking request counts per window.
+  - `limit_keys`: Redis keys storing the configured limit values.
+  - `ttls`: TTLs (in seconds) for each counter key’s window.
+  """
+  @spec check_and_increment(keys, keys, ttls) :: limit_check_result()
+  def check_and_increment(counter_keys, limit_keys, ttls) do
+    script =
+      """
+      for i = 1, #KEYS do
+        local count = tonumber(redis.call("GET", KEYS[i]) or "0")
+        local limit = tonumber(redis.call("GET", ARGV[i]) or "0")
+
+        if count >= limit then
+          local ttl = redis.call("TTL", KEYS[i])
+          return {"throttled", ttl}
+        end
+      end
+
+      -- passed all checks, now increment + expire if needed
+      for i = 1, #KEYS do
+        local ttl = tonumber(ARGV[i + #KEYS])
+        local count = redis.call("INCR", KEYS[i])
+        if count == 1 then
+          redis.call("EXPIRE", KEYS[i], ttl)
+        end
+      end
+
+      return {"allowed"}
+      """
+
+    List.flatten([
+      "EVAL",
+      script,
+      # how many from the list of keys will be in KEYS, 
+      # other will be in ARGV 
+      to_string(length(counter_keys)),
+      counter_keys,
+      limit_keys,
+      Enum.map(ttls, &to_string/1)
+    ])
   end
 end
