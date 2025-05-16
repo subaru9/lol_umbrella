37edf7d84bdb1b3daabd09330d2f15931febedee
Dmytro <subaru9.dev@proton.me>
2025-05-16 10:12:05 +0300

    fix(rl): redis/redix error messages more readable

--- apps/shared_utils/lib/redis/error_converter.ex
+++ apps/shared_utils/lib/redis/error_converter.ex
@@ -7,14 +7,14 @@ defmodule SharedUtils.Redis.Error do
   @spec to_error_message(Redix.Error.t()) :: ErrorMessage.t()
   @spec to_error_message(any()) :: ErrorMessage.t()
   def to_error_message(%{__struct__: Redix.ConnectionError, reason: reason}) do
-    ErrorMessage.internal_server_error("Redis connection error", %{reason: reason})
+    ErrorMessage.internal_server_error("Redis connection error", reason)
   end
 
   def to_error_message(%{__struct__: Redix.Error, message: message}) do
-    ErrorMessage.internal_server_error("Redis command error", %{message: message})
+    ErrorMessage.internal_server_error("Redis command error", message)
   end
 
   def to_error_message(other) do
-    ErrorMessage.internal_server_error("Unknown error", %{error: other})
+    ErrorMessage.internal_server_error("Unknown error", other)
   end
 end
